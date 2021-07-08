ad55e74bd3c2b4a4cd949231c11f0730fd0f933b
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-07 20:20:46 -0700

    added users and user queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,19 @@
-const resolvers = {
+const { User, Book } = require('../models');
 
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('savedBooks');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('savedBooks');
+        }
+
+    } 
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,24 @@ const { gql } = require('apollo-server-express');
 
 // create typeDefs
 const typeDefs = gql`
+    type User {
+       _id: ID
+       username: String
+       email: String
+       password: String
+       savedBooks: [Book]
+       bookCount: Int
+    }
+ 
+    type Book {
+        _id: ID
+    }
 
+    type Query {
+        users: [User]
+        user(username: String!): User
+    }
+    
 `;
 
 module.exports = typeDefs;
