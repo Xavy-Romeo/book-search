eb876c854b39c97dd8177ca528c5fdeb49c674f7
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-10 11:54:52 -0700

    removed express routes

--- server/server.js
+++ server/server.js
@@ -3,10 +3,8 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 
-
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -25,8 +23,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
